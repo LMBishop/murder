87d74b50bf443bf199be05bd03afdca6ece082ff
Leonardo Bishop <me@leonardobishop.com>
2023-08-02 17:36:42 +0100

    Add temporary tablist

---
+++ code/ui/overlay/TabListEntry.cs
@@ -0,0 +1,54 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace MurderGame;
+
+public class TabListEntry : Panel
+{
+	
+	public IClient Client;
+
+	public Label PlayerName;
+	public Label Ping;
+	
+	RealTimeSince TimeSinceUpdate = 0;
+	
+	public TabListEntry()
+	{
+		AddClass( "entry" );
+
+		PlayerName = Add.Label( "PlayerName", "name" );
+		Ping = Add.Label( "", "ping" );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( !IsVisible )
+			return;
+
+		if ( !Client.IsValid() )
+			return;
+
+		if ( TimeSinceUpdate < 0.1f )
+			return;
+
+		TimeSinceUpdate = 0;
+		UpdateData();
+	}
+	
+	public virtual void UpdateData()
+	{
+		PlayerName.Text = Client.Name;
+		Ping.Text = Client.Ping.ToString();
+	}
+	
+	public virtual void UpdateFrom( IClient client )
+	{
+		Client = client;
+		UpdateData();
+	}
+	
+}
